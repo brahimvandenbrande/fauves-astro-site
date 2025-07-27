5b6018e53059fb980f8bfad2d755846ebb6adf80
Brahim Van den Brande <brahim@mobilstudio.be>
2025-07-27 13:25:45 +0200

    Initial commit – Astro site with Degular

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { SITE_URL } from './src/consts.ts';
+import sitemap from '@astrojs/sitemap';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: SITE_URL,
+  integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
---
+++ src/consts.ts
@@ -0,0 +1,4 @@
+// Site wide constants
+
+export const SITE_TITLE = 'Fauves Agency';
+export const SITE_URL = 'https://fauves.agency';
